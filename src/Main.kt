5b950dc18861770f66fd7ab8680459ee6b3a46ca
Sebastian Téllez <sebastian.tellez.o@gmail.com>
2017-09-26 21:10:50 -0500

    Conceptos avanzados de Kotlin: Diferencias entre Kotlin y Java

---
+++ src/Main.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+
+}
+
+data class PersonaKT(val nombre: String, val edad: Int)
---
+++ src/Persona.java
@@ -0,0 +1,43 @@
+public class Persona {
+    String nombre;
+    int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
