ad965230434ab22807b61c1fd19642403d8e1cdc
Sebastian Téllez <sebastian.tellez.o@gmail.com>
2017-09-26 20:25:15 -0500

    Descubriendo Kotlin: Rangos

---
+++ src/rangos.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+//    for (i in 1..4) {
+//        print(i)
+//    }
+
+//    for (i in 4 downTo 1) {
+//        print(i)
+//    }
+
+//    for (i in 1..4 step 2) {
+//        print(i)
+//    }
+
+//    for (i in 4 downTo 1 step 2) {
+//        print(i)
+//    }
+
+//    for (i in 1 until 10) {
+//        print(i)
+//    }
+
+    val esOnce = (1..12 step 2).last == 12
+    print(esOnce)
+
+}
