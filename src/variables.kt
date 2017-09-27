ac08d9e97f6b2d608db02d230f0855fabae5ffe0
Sebastian Téllez <sebastian.tellez.o@gmail.com>
2017-09-26 20:06:27 -0500

    Descubriendo Kotlin: Variables y tipos en Kotlin

---
+++ src/variables.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+
+    val a = 5
+    var b = 4
+
+//    a = 4
+    b = 8
+
+    val nombre:String = "Pepe"
+    b = "9".toInt()
+
+    val num:Int = 0
+    val lng: Long = 23L
+    val lng3 = 23L
+    val flt = 4f
+    val flt2:Float = 4f
+    val doub:Double = 23.9
+
+    val isCool:Boolean = false
+
+}
