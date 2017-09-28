4d2f56f67b70d6901de91e358197bdb599ffdfb6
Sebastian Téllez <sebastian.tellez.o@gmail.com>
2017-09-28 13:22:54 -0500

    Interoperabilidad: Kotlin en Java: funciones

--- KotlinyJava/app/src/main/java/com/platzi/kotlinyjava/MainActivity.java
+++ KotlinyJava/app/src/main/java/com/platzi/kotlinyjava/MainActivity.java
@@ -2,12 +2,39 @@ package com.platzi.kotlinyjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    EditText edtxUser;
+    EditText edtxPass;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtxUser = (EditText) findViewById(R.id.edtxLoginUsuario);
+        edtxPass = (EditText) findViewById(R.id.edtxLoginPassw);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean isValid = ValidacionesKt.passwordValida(edtxPass);
+                Log.e(TAG, "onClick: " + isValid);
+                if (isValid) {
+                    //iniciarVista
+                } else {
+                    Toast.makeText(MainActivity.this, "Password Invalida", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
