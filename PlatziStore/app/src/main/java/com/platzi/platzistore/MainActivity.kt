eb38edb65454152258486305d031220f7b8284a6
Sebastian Téllez <sebastian.tellez.o@gmail.com>
2017-10-02 22:33:30 -0500

    Kotlin en el servidor: Creando el cliente de retrofit para consumir el servicio

--- PlatziStore/app/src/main/java/com/platzi/platzistore/MainActivity.kt
+++ PlatziStore/app/src/main/java/com/platzi/platzistore/MainActivity.kt
@@ -3,7 +3,14 @@ package com.platzi.platzistore
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,11 +20,28 @@ class MainActivity : AppCompatActivity() {
 
         rcViewLanding.layoutManager = GridLayoutManager(this, 2)
 
-        val data = (0..20).map {
-            ItemLanding("Title $it", "Descr $it", 200.00 + it, "")
-        }
+        val retrofit = Retrofit.Builder()
+                .baseUrl("http://10.0.3.2:5000/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
 
-        rcViewLanding.adapter = AdapterLanding(data)
+        val endpoint = retrofit.create(Endpoints::class.java)
+
+        val call = endpoint.getList()
+
+        call.enqueue(object : Callback<JSONObject> {
+
+            override fun onResponse(call: Call<JSONObject>?, response: Response<JSONObject>?) {
+                if (response?.code() == 200) {
+                    Log.e("Respuesta", "${response.body().toString()}")
+                }
+            }
+
+            override fun onFailure(call: Call<JSONObject>?, t: Throwable?) {
+
+            }
+
+        })
 
     }
 }
