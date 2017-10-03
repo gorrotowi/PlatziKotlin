7bd6a48613be5dc6c87d4ce95bc3caa867a22fb1
Sebastian Téllez <sebastian.tellez.o@gmail.com>
2017-10-03 02:32:34 -0500

    Kotlin en el servidor: Hello Ktor

--- src/main/kotlin/AppServer.kt
+++ src/main/kotlin/AppServer.kt
@@ -6,5 +6,12 @@ import org.jetbrains.ktor.response.*
 import org.jetbrains.ktor.routing.*
 
 fun main() {
-
+    val server = embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!", ContentType.Text.Html)
+            }
+        }
+    }
+    server.start(wait = true)
 }
