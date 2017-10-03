a590348cc234324bd159bfcb6963952e795851ae
Sebastian Téllez <sebastian.tellez.o@gmail.com>
2017-10-02 22:22:26 -0500

    Android Kotlin: Implementando la base de datos SQLite de PlatziStore

--- PlatziStore/app/src/main/java/com/platzi/platzistore/DetailActivity.kt
+++ PlatziStore/app/src/main/java/com/platzi/platzistore/DetailActivity.kt
@@ -3,6 +3,7 @@ package com.platzi.platzistore
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.content_descr.*
+import org.jetbrains.anko.db.insert
 import org.jetbrains.anko.startActivity
 
 class DetailActivity : AppCompatActivity() {
@@ -10,6 +11,8 @@ class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        val db = DBOpenHelper.getInstance(this)
 
         intent?.extras?.let {
             val titl = it.getString("title")
@@ -20,6 +23,12 @@ class DetailActivity : AppCompatActivity() {
             txtDetailPrice.text = "$ ${String.format("%.2f", price)}"
 
             btnDetailBuy.setOnClickListener {
+                db?.use {
+                    val namePr = "name" to titl
+                    val descPr = "Desc" to decr
+                    val pricePr = "price" to price
+                    insert("Productos", namePr, descPr, pricePr)
+                }
                 startActivity<ShopCartActivity>()
             }
 
--- PlatziStore/app/src/main/java/com/platzi/platzistore/ShopCartActivity.kt
+++ PlatziStore/app/src/main/java/com/platzi/platzistore/ShopCartActivity.kt
@@ -3,7 +3,9 @@ package com.platzi.platzistore
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_shop_cart.*
+import org.jetbrains.anko.db.select
 
 class ShopCartActivity : AppCompatActivity() {
 
@@ -22,5 +24,30 @@ class ShopCartActivity : AppCompatActivity() {
 
         rcCart.adapter = AdapterCart(items)
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val db = DBOpenHelper.getInstance(this)
+        db?.use {
+            select("Productos").exec {
+                Log.e("Columnas:", "${this.columnCount}")
+                Log.e("Columnas:", "${this.columnNames.size}")
+
+                (this.columnNames).map {
+                    Log.e("Columna: ", "$it")
+                    Log.e("Columna: ", "${this.getColumnIndex(it)}")
+                    Log.e("Columna: ", "${this.getColumnName(this.getColumnIndex(it))}")
+                }
+
+                this.moveToFirst()
+                do {
+                    Log.e("VALUE", this.getString(1) ?: "")
+                    Log.e("VALUE", this.getString(2) ?: "")
+                    Log.e("VALUE", "${this.getDouble(3)}")
+                } while (this.moveToNext())
+
+            }
+        }
     }
 }
