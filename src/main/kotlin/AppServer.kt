ae36fb7b58d5d0aeaedfce9038f79b57c8305997
Sebastian Téllez <sebastian.tellez.o@gmail.com>
2017-10-03 02:32:04 -0500

    Kotlin en el servidor: Configuración de un proyecto con Ktor

---
+++ src/main/kotlin/AppServer.kt
@@ -0,0 +1,10 @@
+import org.jetbrains.ktor.application.*
+import org.jetbrains.ktor.host.*
+import org.jetbrains.ktor.http.*
+import org.jetbrains.ktor.netty.*
+import org.jetbrains.ktor.response.*
+import org.jetbrains.ktor.routing.*
+
+fun main() {
+
+}
