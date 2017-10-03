86c7ec6ca9901f47a657001fc56ab816ae7fbb50
Sebastian Téllez <sebastian.tellez.o@gmail.com>
2017-10-02 21:29:55 -0500

    Android Kotlin: RecyclerView construcción de la estructura de la vista de principal de PlatziStore

--- PlatziStore/app/src/main/java/com/platzi/platzistore/MainActivity.kt
+++ PlatziStore/app/src/main/java/com/platzi/platzistore/MainActivity.kt
@@ -2,9 +2,6 @@ package com.platzi.platzistore
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.longToast
-import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,19 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        txtView.text = "Hola desde Kotlin"
 
-
-//        Toast.makeText(this, "Mensaje desde un Toast", Toast.LENGTH_LONG).show()
-//        toastShort("Toast corto")
-//        toastLong("Mensaje en toast largo")
-//        toast("Mensaje toast largo", Toast.LENGTH_LONG)
-
-//        toast("Mensaje desde Anko")
-        longToast("Otro mensaje desde Anko")
-
-//        startActivity(Intent(this, DetailActivity::class.java))
-
-        startActivity<DetailActivity>()
     }
 }
