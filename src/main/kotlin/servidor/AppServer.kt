10d93635a9fa83b0e74cc5007a7fb7ddf7b3f1ea
Sebastian Téllez <sebastian.tellez.o@gmail.com>
2017-10-03 02:43:27 -0500

    Kotlin en el servidor: Enviando respuestas desde el servidor - GET y POST

---
+++ src/main/kotlin/servidor/AppServer.kt
@@ -0,0 +1,54 @@
+package servidor
+
+import com.google.gson.GsonBuilder
+import org.jetbrains.ktor.application.Application
+import org.jetbrains.ktor.application.install
+import org.jetbrains.ktor.features.CallLogging
+import org.jetbrains.ktor.features.DefaultHeaders
+import org.jetbrains.ktor.http.ContentType
+import org.jetbrains.ktor.http.HttpStatusCode
+import org.jetbrains.ktor.request.receiveText
+import org.jetbrains.ktor.response.respondText
+import org.jetbrains.ktor.routing.get
+import org.jetbrains.ktor.routing.post
+import org.jetbrains.ktor.routing.routing
+
+
+fun Application.main() {
+    install(DefaultHeaders)
+    install(CallLogging)
+
+    routing {
+
+        get("/me") {
+            val name = call.request.queryParameters["name"]
+            val age = call.request.queryParameters["age"]
+
+            val gson = GsonBuilder().setPrettyPrinting().create()
+
+            if (name != null && age != null) {
+
+                val edad = try {
+                    age.toInt()
+                } catch (e: NumberFormatException) {
+                    0
+                }
+                val me = Persona(name, edad)
+                val jsonRes = gson.toJson(me)
+                call.respondText(jsonRes, ContentType.Application.Json, HttpStatusCode.OK)
+
+            } else {
+                val error = Error(HttpStatusCode.BadRequest.value, "Datos invalidos")
+                call.respondText(gson.toJson(error), ContentType.Application.Json, HttpStatusCode.BadRequest)
+            }
+        }
+
+        post("/postme") {
+            val messagepost = call.receiveText()
+            print(messagepost)
+            call.respondText("Ok", ContentType.Text.Html, HttpStatusCode.OK)
+        }
+
+    }
+
+}
---
+++ src/main/kotlin/servidor/ObjetosServer.kt
@@ -0,0 +1,4 @@
+package servidor
+
+data class Persona(val name: String, val age: Int)
+data class Error(val code: Int, val message: String)
